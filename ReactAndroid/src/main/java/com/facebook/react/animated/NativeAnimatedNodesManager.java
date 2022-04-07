3448f718184765464d0d29c7a9475cb02653fd06
Yulian Glukhenko <yulian@infinite.red>
2022-04-07 15:51:06 -0700

    Removes the errors from NativeAnimatedNodesManager.

--- ReactAndroid/src/main/java/com/facebook/react/animated/NativeAnimatedNodesManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/animated/NativeAnimatedNodesManager.java
@@ -330,17 +330,19 @@ import java.util.Queue;
   public void connectAnimatedNodes(int parentNodeTag, int childNodeTag) {
     AnimatedNode parentNode = mAnimatedNodes.get(parentNodeTag);
     if (parentNode == null) {
-      throw new JSApplicationIllegalArgumentException(
-          "connectAnimatedNodes: Animated node with tag (parent) ["
-              + parentNodeTag
-              + "] does not exist");
+      return;
+      // throw new JSApplicationIllegalArgumentException(
+      //     "connectAnimatedNodes: Animated node with tag (parent) ["
+      //         + parentNodeTag
+      //         + "] does not exist");
     }
     AnimatedNode childNode = mAnimatedNodes.get(childNodeTag);
     if (childNode == null) {
-      throw new JSApplicationIllegalArgumentException(
-          "connectAnimatedNodes: Animated node with tag (child) ["
-              + childNodeTag
-              + "] does not exist");
+      return;
+      // throw new JSApplicationIllegalArgumentException(
+      //     "connectAnimatedNodes: Animated node with tag (child) ["
+      //         + childNodeTag
+      //         + "] does not exist");
     }
     parentNode.addChild(childNode);
     mUpdatedNodes.put(childNodeTag, childNode);
@@ -349,17 +351,19 @@ import java.util.Queue;
   public void disconnectAnimatedNodes(int parentNodeTag, int childNodeTag) {
     AnimatedNode parentNode = mAnimatedNodes.get(parentNodeTag);
     if (parentNode == null) {
-      throw new JSApplicationIllegalArgumentException(
-          "disconnectAnimatedNodes: Animated node with tag (parent) ["
-              + parentNodeTag
-              + "] does not exist");
+      return;
+      // throw new JSApplicationIllegalArgumentException(
+      //     "disconnectAnimatedNodes: Animated node with tag (parent) ["
+      //         + parentNodeTag
+      //         + "] does not exist");
     }
     AnimatedNode childNode = mAnimatedNodes.get(childNodeTag);
     if (childNode == null) {
-      throw new JSApplicationIllegalArgumentException(
-          "disconnectAnimatedNodes: Animated node with tag (child) ["
-              + childNodeTag
-              + "] does not exist");
+      return;
+      // throw new JSApplicationIllegalArgumentException(
+      //     "disconnectAnimatedNodes: Animated node with tag (child) ["
+      //         + childNodeTag
+      //         + "] does not exist");
     }
     parentNode.removeChild(childNode);
     mUpdatedNodes.put(childNodeTag, childNode);
